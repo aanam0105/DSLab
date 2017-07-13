b5161424698b45695f4b46ca60725124e5fc48d9
arfatsk <arfats729@gmail.com>
2017-07-13 12:24:44 +0530

    First Commit:Stack Program

---
+++ stact.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#define MAX_SIZE 10
+
+int stack[MAX_SIZE],top=-1;
+
+int isFull(){
+//returns 1 if stack is full else returns -1
+if MAX_SIZE=1
+return 1;
+else return -1;
+}
+	
+int isEmpty(){
+ //returns 1 if stack is empty else returns -1
+ if MAX_SIZE=1
+ return 1;
+ else return -1;
+}
+
+int peek(){
+	//return element at the top of stack
+	if MAX_SIZE=1
+	return top of stack
+}
+			
+void push(int e){
+	//inserts an element into stack
+int d;
+if (d==1)
+
+}
+				
+void pop(){
+	//deletes an element from top of stack
+	int d;
+	if()
+		
+}
+
+int main(){
+	int choice,e
+	do
+	{
+		
+	return 0;
+}
